9b83e1e192aeb5e0fbaffa09f449fb7ab69cf9e6
salnyed <agorbuncova2004@gmail.com>
2023-04-19 17:06:17 +0400

    Add class files

---
+++ Set.h
@@ -0,0 +1,16 @@
+#ifndef LABS_CPP_4SEM___SET_H_
+#define LABS_CPP_4SEM___SET_H_
+
+#include <iostream>
+
+struct Node
+{
+};
+
+class Tree
+{
+ private:
+ public:
+
+};
+#endif
--- main.cpp
+++ main.cpp
@@ -1,7 +1,9 @@
 #include <iostream>
+#include "Set.cpp"
 
 int main()
 {
+	Set a = Set();
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 
