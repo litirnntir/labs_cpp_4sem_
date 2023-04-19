bb1984c85b03c1bfe9d2772c415c90145a983b89
salnyed <agorbuncova2004@gmail.com>
2023-04-19 22:05:06 +0400

    Add 4 variant task

--- main.cpp
+++ main.cpp
@@ -1,5 +1,83 @@
 #include <iostream>
 #include "Set.cpp"
+#include <vector>
+
+int check()
+{
+	int number;
+	while (!(std::cin >> number) || (std::cin.peek() != '\n'))
+	{
+		std::cin.clear();
+		while (std::cin.get() != '\n');
+		std::cout << "Incorrect value" << std::endl;
+	}
+	return number;
+}
+
+std::vector<int> deleteDuplicate(std::vector<int>& data)
+{
+	std::vector<int> result;
+	bool flag = false;
+	for (auto it = data.begin(); it != data.end(); it++)
+	{
+		for (auto iter = it + 1; iter != data.end(); iter++)
+		{
+			if (*it == *iter)
+			{
+				data.erase(iter--);
+				flag = true;
+			}
+		}
+		if (!flag)
+		{
+			result.push_back(*it);
+		}
+		flag = false;
+	}
+	return result;
+}
+
+void task()
+{
+	int number;
+	bool flag = true;
+	std::vector<int> a;
+	while (flag)
+	{
+		std::cout << "Input number = ";
+		if (!(std::cin >> number) || (std::cin.peek() != '\n'))
+		{
+			std::cin.clear();
+			while (std::cin.get() != '\n');
+			std::cout << "Incorrect value" << std::endl;
+		}
+		else
+		{
+			a.push_back(number);
+		}
+		std::cout << "Continue?" << std::endl;
+		std::cout << "1. yes" << std::endl;
+		std::cout << "2. no" << std::endl;
+		std::cout << "Input choose:";
+		int n = check();
+		while (n > 2 || n <= 0)
+		{
+			std::cout << "Incorrect value" << std::endl << "Input choose:";
+			n = check();
+		}
+		if (n == 1)
+			flag = true;
+		else
+			flag = false;
+	}
+	std::vector<int> tmp;
+	tmp = deleteDuplicate(a);
+	for (int & it : tmp)
+	{
+		std::cout << it;
+	}
+	getchar();
+}
 
 int main()
 {
@@ -12,4 +90,6 @@ int main()
 	a.erase(5);
 	std::cout << a.contains(1);
 	a.print();
+	std::vector<int> b = {1,2,3,4};
+	task();
 }
