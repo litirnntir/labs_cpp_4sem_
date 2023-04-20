c0c672d388194b99d012bfe1f5679aa701173745
salnyed <agorbuncova2004@gmail.com>
2023-04-20 19:19:55 +0400

    Some fix

--- Set.h
+++ Set.h
@@ -25,15 +25,15 @@ class Set
 	Node* _contains(Node* root, int value) const;
 	Node* _insert(Node* root, int data);
 	Node* _erase(Node* root, int key);
+	Node* _balanced(Node* root);
 	void _copy(const Node* obj);
 	void _destructor(Node* obj);
-	Node* _findMin(Node* root);
-	void _fixHeight(Node* p);
-	Node* _balance(Node* root);
 	int _balanceFactor(Node* p);
 	Node* _rotateRight(Node* p);
+	Node* _findMin(Node* root);
+	Node* _balance(Node* root);
 	Node* _rotateLeft(Node* q);
-	Node* _balanced(Node* root);
+	void _fixHeight(Node* p);
  public:
 	Set& operator=(const Set& obj);
 	bool contains(int key) const;
